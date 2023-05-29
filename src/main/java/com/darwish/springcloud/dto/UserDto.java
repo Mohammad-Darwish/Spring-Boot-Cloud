580b3ce45f879f52c5253972938694544b61b647
Mohammad Darwish <mohammad.darwish@ericsson.com>
2023-05-29 15:05:29 +0200

    Customize swaggert schema

--- src/main/java/com/darwish/springcloud/dto/UserDto.java
+++ src/main/java/com/darwish/springcloud/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.darwish.springcloud.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+    description = "UserDto model information"
+)
 @Setter
 @Getter
 @AllArgsConstructor
@@ -15,12 +19,21 @@ public class UserDto {
 
     private Long id;
 
+    @Schema(
+        description = "User First name"
+    )
     @NotEmpty(message = "user first name should not be null or empty")
     private String firstName;
 
+    @Schema(
+        description = "User Last name"
+    )
     @NotEmpty(message = "user last name should not be null or empty")
     private String lastName;
 
+    @Schema(
+        description = "User Email Address"
+    )
     @NotEmpty(message = "user email should not be null or empty")
     @Email(message = "Email address should be well valid")
     private String email;
